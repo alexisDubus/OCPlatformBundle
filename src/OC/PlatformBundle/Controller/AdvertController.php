8430c1cbd4361a05ac269ac71affcbf405729ec4
Alexis Dubus <ielow.submarine@gmail.com>
2017-06-01 23:04:28 +0200

    amélioration design et fonctionnalité

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -15,6 +15,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use OC\PlatformBundle\Entity\Image;
+use OC\PlatformBundle\Form\AdvertType;
 
 class AdvertController extends Controller
 {
@@ -85,31 +86,66 @@ class AdvertController extends Controller
 
     public function addAction(Request $request)
     {
+        $advert = new Advert();
+        $form   = $this->get('form.factory')->create(AdvertType::class, $advert);
+
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($advert);
+            $em->flush();
+
+            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+
+            return $this->redirectToRoute('oc_platform_view', array('id' => $advert->getId()));
+        }
+
+        return $this->render('OCPlatformBundle:Advert:add.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+    public function addActionOldV2(Request $request)
+    {
         // On crée un objet Advert
         $advert = new Advert();
 
-        // On crée le FormBuilder grâce au service form factory
-        $formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $advert);
-
-        // On ajoute les champs de l'entité que l'on veut à notre formulaire
-        $formBuilder
+        // J'ai raccourci cette partie, car c'est plus rapide à écrire !
+        $form = $this->get('form.factory')->createBuilder(FormType::class, $advert)
             ->add('date',      DateType::class)
             ->add('title',     TextType::class)
             ->add('content',   TextareaType::class)
             ->add('author',    TextType::class)
-            ->add('published', CheckboxType::class)
+            ->add('published', CheckboxType::class, array('required' => false))
             ->add('save',      SubmitType::class)
+            ->getForm()
         ;
-        // Pour l'instant, pas de candidatures, catégories, etc., on les gérera plus tard
 
-        // À partir du formBuilder, on génère le formulaire
-        $form = $formBuilder->getForm();
+        // Si la requête est en POST
+        if ($request->isMethod('POST')) {
+            // On fait le lien Requête <-> Formulaire
+            // À partir de maintenant, la variable $advert contient les valeurs entrées dans le formulaire par le visiteur
+            $form->handleRequest($request);
 
-        // On passe la méthode createView() du formulaire à la vue
-        // afin qu'elle puisse afficher le formulaire toute seule
+            // On vérifie que les valeurs entrées sont correctes
+            // (Nous verrons la validation des objets en détail dans le prochain chapitre)
+            if ($form->isValid()) {
+                // On enregistre notre objet $advert dans la base de données, par exemple
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($advert);
+                $em->flush();
+
+                $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+
+                // On redirige vers la page de visualisation de l'annonce nouvellement créée
+                return $this->redirectToRoute('oc_platform_view', array('id' => $advert->getId()));
+            }
+        }
+
+        // À ce stade, le formulaire n'est pas valide car :
+        // - Soit la requête est de type GET, donc le visiteur vient d'arriver sur la page et veut voir le formulaire
+        // - Soit la requête est de type POST, mais le formulaire contient des valeurs invalides, donc on l'affiche de nouveau
         return $this->render('OCPlatformBundle:Advert:add.html.twig', array(
-            'advert'           => $advert,
-            'form' => $form->createView()
+            'form' => $form->createView(),
         ));
     }
 
